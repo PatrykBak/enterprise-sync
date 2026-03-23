541e82a4799118168781c4fab5a0b15b2342588a
PatrykBak <bakevan@gmail.com>
2026-03-23 10:55:08 +0100

    Services versions correction, update start-dev.sh to handle healthy.

--- start-dev.sh
+++ start-dev.sh
@@ -15,17 +15,24 @@ wait_for_healthy_service() {
   local service_name=$1
   echo "Waiting for $service_name to be healthy..."
   
-  local container_id=$(docker compose ps -q $service_name)
-  
-  status=$(docker inspect --format "{{json .State.Health.Status }}" $container_id)
-  while [ "$status" != "\"healthy\"" ]; do
-    if [ "$status" == "\"unhealthy\"" ]; then
-      echo "Error: $service_name is unhealthy!"
-      exit 1
-    fi
+  local MAX_RETRIES=60
+  local RETRIES=0
+  local status=""
+
+  while [ "$status" != "healthy" ]; do
     sleep 2
     echo -n "."
-    status=$(docker inspect --format "{{json .State.Health.Status }}" $container_id)
+    
+    local container_id=$(docker compose ps -q "$service_name")
+    if [ -n "$container_id" ]; then
+        status=$(docker inspect -f '{{.State.Health.Status}}' "$container_id")
+    fi
+
+    RETRIES=$((RETRIES+1))
+    if [ $RETRIES -ge $MAX_RETRIES ]; then
+        echo -e "\nError: $service_name failed to become healthy in time."
+        exit 1
+    fi
   done
   
   echo "$service_name is ready!"
